0f54c678daa4930d55b2cfa0ad9e501f6816c011
Ddennc <166750269+Ddennc@users.noreply.github.com>
2024-08-21 19:16:12 +0200

    first commit

---
+++ src/main/java/org/example/spring_data_rest_/SpringDataRestApplication.java
@@ -0,0 +1,13 @@
+package org.example.spring_data_rest_;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/spring_data_rest_/SpringDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring_data_rest_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
