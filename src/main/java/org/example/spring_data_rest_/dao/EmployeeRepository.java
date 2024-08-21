e9faec22a40b48220ced0ffc8645b6a78c8f5f98
Ddennc <166750269+Ddennc@users.noreply.github.com>
2024-08-21 19:16:46 +0200

    Spring data rest

---
+++ src/main/java/org/example/spring_data_rest_/dao/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package org.example.spring_data_rest_.dao;
+
+
+
+
+
+
+import org.example.spring_data_rest_.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+// public List<Employee> findAllByName(String name);
+}
